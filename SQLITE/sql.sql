b3e21a4554834c6093e8d20efbffcd592948a225
fahmy <fahmyniaz@gmail.com>
2022-11-30 22:44:36 +0530

    first commit

---
+++ SQLITE/sql.sql
@@ -0,0 +1,7 @@
+--CREATE TABLE Teams (Team_ID TEXT NOT NULL PRIMARY KEY,
+--Name, Project_Name,About);
+
+--INSERT INTO Teams( Team_ID,Name,Project_Name,About)
+--VALUES("0004","TeamFour","ProjectFour","Dragon Project of Dragons 4");
+
+SELECT * FROM Teams;
